8ba9f27eda7e8815d1558466100508e226f576ae
imanirvansyah <imanirvansyaah@gmail.com>
2025-06-28 15:24:51 +0700

    add 3d assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client"
 import { MouseIcon } from '@/components/icons/mouse'
 import { DescriptionSection } from '@/components/sections/description'
 import { FeaturesSection } from '@/components/sections/features'
@@ -6,9 +9,15 @@ import { HeroSection } from '@/components/sections/hero'
 import { SpecSection } from '@/components/sections/specs'
 import { SupportAppSection } from '@/components/sections/support-app'
 
+import { OrbitControls, useGLTF } from '@react-three/drei';
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from 'three';
+
 export default function Home() {
   return (
     <div>
+      <Canvas3D />
       <HeroSection />
       <DescriptionSection />
       <FeaturesSection />
@@ -21,4 +30,66 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+
+export const Canvas3D = () => {
+  return (
+    <div id="canvas-container" style={{ width: '100vw', height: '100vh', position: 'fixed', top: 0, left: 0 }}>
+      <Canvas>
+        {/* <ambientLight intensity={0.2} /> */}
+        <directionalLight />
+        {/* <OrbitControls /> */}
+        <Box />
+        {/* <Ico /> */}
+      </Canvas>
+    </div>
+  )
+}
+
+const Box = () => {
+  const meshRef = useRef<THREE.Mesh>(null!);
+
+  useFrame((_, delta) => {
+    if (!meshRef) return;
+
+    meshRef.current.rotation.x += delta
+    meshRef.current.rotation.y += delta
+  })
+
+
+  const { nodes, materials } = useGLTF('/models/black-ring.glb');
+
+  return (
+    /* @ts-ignore */
+    <group ref={meshRef} isGroup>
+      <mesh
+        /* @ts-ignore */
+        geometry={nodes?.Ring.geometry}
+        scale={2}
+        material={materials["darkBlue"]}
+        position={[0, 0, 0]}>
+        {/* <boxGeometry /> */}
+      </mesh>
+      <mesh
+        /* @ts-ignore */
+        geometry={nodes?.Ring2.geometry}
+        scale={2}
+        material={materials["Black"]}
+        position={[0, .001, 0]}>
+      </mesh>
+      <mesh
+        /* @ts-ignore */
+        geometry={nodes?.Ring001.geometry}
+        scale={2}
+        material={materials["darkBlue"]}
+        position={[0, .002, 0]}>
+      </mesh>
+    </group>
+    // <mesh ref={meshRef} position={[0, 0, 0]} >
+    //   <ambientLight intensity={0.2} />
+    //   <boxGeometry args={[1, 1, 1]} />
+    //   <meshNormalMaterial />
+    // </mesh>
+  )
 }
