40b9e4b857d6367a5cf3ab8d03b5186711c13021
imanirvansyah <imanirvansyaah@gmail.com>
2025-06-30 16:12:57 +0700

    fix build

---
+++ src/components/sections/3d-section/index.tsx
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client"
+
+import { useGLTF } from '@react-three/drei';
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from 'three';
+
+export const Canvas3D = () => {
+  return (
+    <div id="canvas-container" style={{ width: '100vw', height: '100vh', position: 'fixed', top: 0, left: 0 }}>
+      <Canvas>
+        {/* <ambientLight intensity={0.2} /> */}
+        <directionalLight />
+        {/* <OrbitControls /> */}
+        <Box />
+        {/* <Ico /> */}
+      </Canvas>
+    </div>
+  )
+}
+
+const Box = () => {
+  const meshRef = useRef<THREE.Mesh>(null!);
+
+  useFrame((_, delta) => {
+    if (!meshRef) return;
+
+    meshRef.current.rotation.x += delta
+    meshRef.current.rotation.y += delta
+  })
+
+
+  const { nodes, materials } = useGLTF('/models/black-ring.glb');
+
+  return (
+    /* @ts-ignore */
+    <group ref={meshRef} isGroup>
+      <mesh
+        /* @ts-ignore */
+        geometry={nodes?.Ring.geometry}
+        scale={2}
+        material={materials["darkBlue"]}
+        position={[0, 0, 0]}>
+        {/* <boxGeometry /> */}
+      </mesh>
+      <mesh
+        /* @ts-ignore */
+        geometry={nodes?.Ring2.geometry}
+        scale={2}
+        material={materials["Black"]}
+        position={[0, .001, 0]}>
+      </mesh>
+      <mesh
+        /* @ts-ignore */
+        geometry={nodes?.Ring001.geometry}
+        scale={2}
+        material={materials["darkBlue"]}
+        position={[0, .002, 0]}>
+      </mesh>
+    </group>
+  )
+}
